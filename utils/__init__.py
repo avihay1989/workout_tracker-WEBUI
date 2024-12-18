edbc9b5e520acb3035e63499467d1a043437e0dc
avihay1989 <58347128+avihay1989@users.noreply.github.com>
2024-12-18 18:37:02 +0200

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# utils/__init__.py
-from .config import DB_FILE, LOGS_DIR
-from .helpers import (
-    calculate_total_sets,
-    calculate_fractional_sets,
-    calculate_direct_sets,
-)
-from .data_handler import DataHandler
-from .muscle_group import MuscleGroupHandler
